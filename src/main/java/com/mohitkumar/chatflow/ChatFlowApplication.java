eb1db3eade0f95116007252cc7862648ce6f345e
wreckurring <wreckurring@gmail.com>
2026-03-10 02:26:31 +0530

    fix: add spring retry to prevent data loss

--- src/main/java/com/mohitkumar/chatflow/ChatFlowApplication.java
+++ src/main/java/com/mohitkumar/chatflow/ChatFlowApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableRetry
 public class ChatFlowApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatFlowApplication.class, args);
